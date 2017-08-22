6c386f5308ac7078b0a3a7a2129d4a9d350582e6
Nikolay <artamonov.nikolay@e-queo.com>
2017-08-22 18:21:22 +0300

    add table

--- views/forecast/weather-get-success.php
+++ views/forecast/weather-get-success.php
@@ -6,14 +6,17 @@ use yii\helpers\VarDumper;
 ActiveForm::begin();
 
 ?>
-		<table>
+		<table cellspacing="5" cellpadding="5" border="1" > 
 		<?php 
-				for($i = 0; $i < sizeof($weather); $i++) {
-						array_map(function($day) {
-							echo	Html::encode("Temp: {$day['averageTempC']} ");
-						}, $weather[$i]);
-						echo "<br>";
-				}
+			array_map(function($week) {
+							echo "<tr>";
+							array_map(function($day) {
+								echo "<td>";
+								echo Html::encode("Temp: {$day['averageTempC']}");
+								echo "</td>";
+							}, $week);
+							echo "</tr><br/>";
+				}, $weather);
 		?>
 		</table>
 
